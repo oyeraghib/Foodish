1deb7f2c0af54df00199045c9edd2fcd2f1783f4
oyeraghib <raghib.khan76@gmail.com>
2022-07-13 09:45:03 +0530

    navigate between fragments working

--- app/src/main/java/com/example/foodx/MainActivity.kt
+++ app/src/main/java/com/example/foodx/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.foodx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+
+        navController = findNavController(R.id.fragmentContainer)
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipeFragment,
+                R.id.favoriteFragment,
+                R.id.jokeFragment
+            )
+        )
+        navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
