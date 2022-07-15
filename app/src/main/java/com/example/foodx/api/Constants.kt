b4ed20a9563d5b02596e1c37038822a21d457dce
oyeraghib <raghib.khan76@gmail.com>
2022-07-15 21:41:40 +0530

    add: constants for base url & api key

---
+++ app/src/main/java/com/example/foodx/api/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.foodx.api
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com/"
+        const val API_KEY = "56ba91a9812d4c2aa38e2880d48d3755"
+    }
+}
