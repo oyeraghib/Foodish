384af875efc408f151f9e943dc0ea21cee242db1
oyeraghib <raghib.khan76@gmail.com>
2022-07-22 20:17:07 +0530

    add: Timber for debugging

---
+++ app/src/main/java/com/example/foodx/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.foodx
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
