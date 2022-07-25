8b30e126a4cf36e0f78401893d28fdc87be26f93
oyeraghib <raghib.khan76@gmail.com>
2022-07-25 23:55:08 +0530

    add: Network Module to get API Service
    Convert the FoodRecipeAPI class to get response from it using Retrofit which we Inject using Hilt

---
+++ app/src/main/java/com/example/foodx/api/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.example.foodx.api.di
+
+import com.example.foodx.api.Constants
+import com.example.foodx.api.service.FoodRecipeAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().readTimeout(2, TimeUnit.MINUTES)
+            .writeTimeout(2, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): MoshiConverterFactory {
+        return MoshiConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipeAPI {
+        return retrofit.create(FoodRecipeAPI::class.java)
+    }
+}
