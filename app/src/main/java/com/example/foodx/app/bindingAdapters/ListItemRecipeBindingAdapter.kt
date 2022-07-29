0eb41bf3113f722ca1e376d326c5f76d1486efb3
oyeraghib <raghib.khan76@gmail.com>
2022-07-30 03:21:45 +0530

    refactor: use data binding adapters to inflate data

---
+++ app/src/main/java/com/example/foodx/app/bindingAdapters/ListItemRecipeBindingAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.foodx.app.bindingAdapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.example.foodx.R
+
+class ListItemRecipeBindingAdapter {
+
+    companion object {
+        @BindingAdapter("applyVegetarianColor")
+        @JvmStatic
+        fun applyVegetarianColor(view: View, vegan: Boolean) {
+            if (vegan) {
+                when (view) {
+                    is TextView -> {
+                        view.setTextColor(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+
+                    is ImageView -> {
+                        view.setColorFilter(
+                            ContextCompat.getColor(
+                                view.context,
+                                R.color.green
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
