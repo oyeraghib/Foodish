db820e0358f50edb10bec17d7c3d5bbfe5e9e532
oyeraghib <raghib.khan76@gmail.com>
2022-08-14 08:55:01 +0530

    add network listener to check for internet

---
+++ app/src/main/java/com/example/foodx/app/utils/NetworkListener.kt
@@ -0,0 +1,43 @@
+package com.example.foodx.app.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@Suppress("DEPRECATION")
+class NetworkListener : ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable = MutableStateFlow(false)
+
+    fun checkNetworkAvailability(context: Context): MutableStateFlow<Boolean> {
+
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        //MinSdk Changed to 24 to use {@registerDefaultNetworkCallback}
+        connectivityManager.registerDefaultNetworkCallback(this)
+
+        var isConnected = false
+
+        connectivityManager.allNetworks.forEach { network ->
+            val networkCapability = connectivityManager.getNetworkCapabilities(network)
+            networkCapability.let {
+                if (it!!.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET))
+                    isConnected = true
+                return@forEach
+            }
+        }
+        isNetworkAvailable.value = isConnected
+        return isNetworkAvailable
+    }
+
+    override fun onAvailable(network: Network) {
+        isNetworkAvailable.value = true
+    }
+
+    override fun onLost(network: Network) {
+        isNetworkAvailable.value = false
+    }
+}
